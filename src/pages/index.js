aa72a74b57558dbf51959638fda7cd90b44a6f65
edwardz8 <tolklorist@gmail.com>
2020-05-29 22:45:05 -0400

    adjust header

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ class IndexPage extends React.Component {
         <div style={{textAlign: "center", marginTop: "20rem"}}>
         <h1 style={{color: "white", fontSize: "64px"}}>Tavern</h1>
         <p style={{color: "white", fontSize: "24px", margin: "0 auto", width: "50%"}}>
-          A Software Development Production House focused in React Native tvOS + mobile apps.
+          A Software Development Production House focused in React Native
         </p>
         </div>
       </div>

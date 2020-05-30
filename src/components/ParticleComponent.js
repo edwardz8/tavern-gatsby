041e42031f8039126ffd8aee926be0bcc6e7b652
edwardz8 <tolklorist@gmail.com>
2020-05-29 22:26:04 -0400

    initial setup

--- src/components/ParticleComponent.js
+++ src/components/ParticleComponent.js
@@ -11,7 +11,6 @@ export default () => (
   >
     <Particles
       // Add window resize logic if required
-      height={window.outerHeight}
       params={{
         particles: {
           number: {
